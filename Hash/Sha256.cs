4888b53661b30394d9cd2145caa76e323aa057b3
Hugo Forte <hforte@gcmlp.com>
2015-02-17 13:34:25 -0600

    Added Sha256 Implementation

---
+++ Hash/Sha256.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Crypto
+{
+    public class Sha256 : IGenerateHash
+    {
+        public string GetHash(object toHash)
+        {
+            var json = JsonConvert.SerializeObject(toHash);
+            return GetHash(json);
+        }
+
+        public string GetHash(string toHash)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(toHash);
+            var hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            return hash.Aggregate(string.Empty, (current, x) => current + String.Format("{0:x2}", x));
+        }
+    }
+
+    public interface IGenerateHash
+    {
+        string GetHash(object toHash);
+        string GetHash(string toHash);
+    }
+}
---
+++ UnitTest/Sha256Tests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crypto;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class Sha256Tests
+    {
+        private Sha256 _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Sha256();
+        }
+
+        [Test]
+        public void CanComputeKnownHash()
+        {
+            const string toComputeFrom = "SHA-256 hash to be converted";
+            const string knownHash = "c8e23368e5dd263cf0c4617930c2c814e3038a4784b7db0967f7c74375536c35";
+
+            var result = _sut.GetHash(toComputeFrom);
+            Assert.AreEqual(result, knownHash);
+        }
+
+        [Test]
+        public void CanComputeHashForObject()
+        {
+            var testObject = new ParentTestClass
+            {
+                ParentName = Guid.NewGuid().ToString(),
+                ChildrenList = new List<ChildTestClass>
+                {
+                    new ChildTestClass
+                    {
+                        ChildName = Guid.NewGuid().ToString()
+                    },
+                    new ChildTestClass
+                    {
+                        ChildName = Guid.NewGuid().ToString()
+                    },
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(testObject);
+            var knownHash = _sut.GetHash(json);
+
+            var result = _sut.GetHash(testObject);
+            Assert.AreEqual(knownHash, result);
+
+        }
+
+        public class ParentTestClass
+        {
+            public string ParentName { get; set; }
+            public List<ChildTestClass> ChildrenList { get; set; }
+        }
+
+        public class ChildTestClass
+        {
+            public string ChildName { get; set; }
+        }
+    }
+}
